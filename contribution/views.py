ee97e9265d573a526d602a0921e5cc4e663e7ed7
otasu <auraroundwithstar@gmail.com>
2020-02-22 16:16:59 +0900

    Modified template to display from database

--- contribution/views.py
+++ contribution/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone
+
+from .models import Contribution
 
 def post_list(request):
-    return render(request, 'contribution/post_list.html')
+    contributions = Contribution.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+    return render(request, 'contribution/post_list.html', {'contributions': contributions})
